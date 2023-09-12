3b49c4ef83cc2b10d389b3df43423056ab5c2010
J-HernandezM <juhernandezm@unal.edu.co>
2023-09-12 17:48:52 -0500

    fix: font issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Header from './Components/Header'
@@ -9,13 +8,13 @@ function App() {
   return (
     <HashRouter>
       <Header />
-
       <Routes>
         <Route path='/' element={<main>Homepage</main>}/>
         <Route path='/category' element={<main>Category display</main>} />
         <Route path='/category/product' element={<main>Producto</main>} />
         <Route path='*' element={<main>404 Not found</main>}/>
       </Routes>
+      <p>hola</p>
 
       <Footer />
     </HashRouter>
