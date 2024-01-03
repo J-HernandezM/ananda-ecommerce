161448cc086cf06d0c837c29e954e2fd69dc13d5
Santiago <scb9908@gmail.com>
2024-01-02 23:15:13 -0500

    feat: create tests for sanitizeApiResponse

--- src/shared/sanitizeApiResponse.js
+++ src/shared/sanitizeApiResponse.js
@@ -3,26 +3,25 @@ export const sanitizeApiResponse = (response) => {
     return;
   }
 
-  if (Array.isArray(response.data)) {
-    const sanitized = response.data.reduce((acc, curr) => {
-      const featuredImage = curr.attributes.featuredImage.data.attributes;
-      const images = curr.attributes.images.data.map((i) => i.attributes);
-
-      const item = {
-        id: curr.id,
-        ...curr.attributes,
-        featuredImage,
-        images,
-      };
-      acc.push(item);
-      return acc;
-    }, []);
-    return sanitized;
+  if (!Array.isArray(response.data)) {
+    return;
   }
 
-  const sanitized = {
-    id: response.data.id,
-    ...response.data.attributes,
-  };
+  const sanitized = response.data.reduce((acc, curr) => {
+    const featuredImage = curr.attributes.featuredImage.data.attributes;
+    const images = curr.attributes.images.data.map((i) => i.attributes);
+
+    const item = {
+      id: curr.id,
+      ...curr.attributes,
+      featuredImage,
+      images,
+    };
+
+    acc.push(item);
+
+    return acc;
+  }, []);
+
   return sanitized;
 };
--- src/shared/tests/sanitizeApiResponse.test.js
+++ src/shared/tests/sanitizeApiResponse.test.js
@@ -22,4 +22,22 @@ describe('sanitizeApiResponse', () => {
 
     expect(result).toEqual(expect.arrayContaining([expected]));
   });
+
+  it('Should be empty if API response is invalidad', () => {
+    const invalidData = { data: undefined };
+
+    const expected = undefined;
+    const result = sanitizeApiResponse(invalidData);
+
+    expect(expected).toBe(result);
+  });
+
+  it('Should be empty if API response is not an array', () => {
+    const invalidData = { data: 123123 };
+
+    const expected = undefined;
+    const result = sanitizeApiResponse(invalidData);
+
+    expect(expected).toBe(result);
+  });
 });
